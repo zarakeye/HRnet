1755f0c3f70f942be81972bb3427a27a81488a8f
Stéphane Bazzé <stephane.bazze@outlook.fr>
2025-08-08 11:52:20 +0200

    try to fix cors policy problem

--- src/app/hooks/useDbStatus.ts
+++ src/app/hooks/useDbStatus.ts
@@ -20,8 +20,7 @@ export function useDbStatus() {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json',
-          },
-          cache: 'no-store',
+          }
         });
 
         if (!response.ok) throw new Error('DB not ready');
